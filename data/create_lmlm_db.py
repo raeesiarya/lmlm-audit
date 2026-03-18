ca221820556601497a88b36c89ae184aa3b8ddbb
aryaraeesi <aryaraeesi@blinkweb.no>
2026-03-18 15:54:10 -0700

    added lmlm db

---
+++ data/create_lmlm_db.py
@@ -0,0 +1,23 @@
+from datasets import load_dataset
+import os
+from dotenv import load_dotenv
+import json
+from tqdm import tqdm
+
+
+def create_lmlm_database() -> None:
+    load_dotenv()
+    hf_token = os.getenv("HF_TOKEN")
+
+    ds = load_dataset("kilian-group/LMLM-database", token=hf_token)
+
+    triplets = ds["test"][0]["triplets"]
+
+    with open("data/lmlm_database.jsonl", "w") as f:
+        for i, (s, r, o) in enumerate(tqdm(triplets, desc="Saving triplets")):
+            json.dump({"id": i, "s": s, "r": r, "o": o}, f)
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    create_lmlm_database()
